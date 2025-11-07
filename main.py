59394233acd233bb61df82bdd3086b29f957a762
hellosamyak <jainsamyak0805@gmail.com>
2025-11-07 18:32:46 +0530

    add vercel url

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="AgriPulse Backend")
 # ✅ Allow frontend access
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://agri-pulse-frontend.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
